21610695de6ba00fe5a3ff2e0822c7304ccb3566
Abdurore <137651522+Abdurore@users.noreply.github.com>
2025-10-29 12:33:57 +0100

    Update main.jsx

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -2,10 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+
   <StrictMode>
     <App />
+    <Toaster
+    position='top-right'
+    toastOptions={{
+      duration:4000,
+      style:{
+        background:'#22c55e',
+        color:'white',
+        fontFamily:'"Courier New", monospace',
+        fontWeight:'bold',
+        marginTop:'5px',
+        marginRight:'5px',
+      },
+      success:{
+        icon:'★'
+      },
+      error:{
+        icon:'✕'
+      },
+    }}
+    />
   </StrictMode>,
 )
+
+
 
