3354d25b28c54dfbe3e9665777be4df12d1e531e
Abdurore <137651522+Abdurore@users.noreply.github.com>
2025-10-29 12:35:22 +0100

    Update Login.jsx

--- React/src/components/Login.jsx
+++ React/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
+import { toast } from 'react-hot-toast'
 
 function Login() {
   const [email, setEmail] = useState('')
@@ -34,5 +34,6 @@ function Login() {
     </div>
   )
 }
+
 
 export default Login
